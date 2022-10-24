fec00239896f64f3d67d12b6bcaf6d7ab22bce3a
zcfabra <christianwgnam@gmail.com>
2022-10-24 10:24:25 -0400

    fixed BIG bug

--- lenfind.py
+++ lenfind.py
@@ -40,25 +40,30 @@ def main():
     pat = re.compile(r'(?:\d+:)+\d+')
     matches = re.findall(pat,text)
     refined = [each for ix, each in enumerate(matches) if ix %2 == 0]
+    print(refined)
     total = 0
     for each in refined:
         proc = each.split(":")
         accum = 0
         for ix, each in enumerate(proc[::-1]):
             if ix == 0:
-                accum += ix
+                accum += int(each)
             if ix == 1:
-                accum += ix * 60
+                print("HIT")
+                accum += int(each) * 60
             if ix == 2:
-                accum += ix * 3600
+                accum += int(each) * 3600
             if ix == 3:
-                accum += ix * 216000
+                accum += int(each) * 216000
+        print(accum)
         total += accum
 
-    print(f"At 1x Speed: {str(datetime.timedelta(seconds=total))}")
-    print(f"At 1.5x Speed: {str(datetime.timedelta(seconds=total/1.5))}")
-    print(f"At 1.75x Speed: {str(datetime.timedelta(seconds=total/1.75))}")
-    print(f"At 2x Speed: {str(datetime.timedelta(seconds=total//2))}")
+    print(total)
+
+    print(f"At 1x Speed: {str(datetime.timedelta(seconds=total)).split('.')[0]}")
+    print(f"At 1.5x Speed: {str(datetime.timedelta(seconds=total/1.5)).split('.')[0]}")
+    print(f"At 1.75x Speed: {str(datetime.timedelta(seconds=total/1.75)).split('.')[0]}")
+    print(f"At 2x Speed: {str(datetime.timedelta(seconds=total//2)).split('.')[0]}")
 
 
         
